96e7517653ec3ff5f529665777e045f0fc6032db
Da Silva Bruno <dasilvabruno86@hotmail.fr>
2017-08-31 19:43:01 +0200

    Waiting + attente active visible V2

--- firmware/project/src/states/WaitingState.cpp
+++ firmware/project/src/states/WaitingState.cpp
@@ -3,8 +3,8 @@
 #include "TFT.h"
 
 unsigned long time;
-const int16_t x = 260;
-int counter = 1;
+const int16_t x = 220;
+int counter = 0;
 
 WaitingState::WaitingState()
 {
@@ -12,7 +12,7 @@ WaitingState::WaitingState()
     tft.fillScreen(WHITE);
     tft.setCursor(40, 100);
     tft.setTextColor(BLACK);
-    tft.setTextSize(5);
+    tft.setTextSize(4);
     tft.setTextWrap(false);
     tft.print("Waiting");
     time = millis();
@@ -28,11 +28,14 @@ int WaitingState::loop()
 
     if (counter == 3)
     {
-        counter = 1;
-        tft.fillRect(x, 110, 20, 10, YELLOW);
+        counter = 0;
+        tft.fillRect(x, 110, 40, 40, WHITE);
     }
-    tft.drawChar(x + counter * 10, 120, '.', BLACK, WHITE, 2);
+ 
+    tft.drawChar(x + counter * 10, 115, '.', BLACK, WHITE, 2);
     counter++;
+
+   
 
     time = millis();
 }
