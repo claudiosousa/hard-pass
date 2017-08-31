8159f0e59a15d0d87a2c2aa06e23a9e924b8c51c
Dimitri Lizzi <dimitri.lizzi@gmail.com>
2017-08-31 19:51:46 +0200

    Automatic detection of serial port

--- bridge/bridge.py
+++ bridge/bridge.py
@@ -1,24 +1,46 @@
 #!/usr/bin/env python3
 
-from bottle import Bottle, run, request, HTTPError
+import bottle
+from bottle import Bottle, run, request, response
 from serial import Serial
+import serial.tools.list_ports
 import argparse
 
 PAYLOAD_TERMINATOR = b'\0'
+KNOWN_DEVICES = {"USB-SERIAL CH340"}
 
 
 class PayloadTooBigException(BaseException):
     pass
 
-
 ser = Serial()
 app = Bottle()
 
+def find_device_port():
+    for port in serial.tools.list_ports.comports():
+        for known_device in KNOWN_DEVICES:
+            if known_device in port.description:
+                return str(port.device)
+
+def enable_cors(fn):
+    def _enable_cors(*args, **kwargs):
+        # set CORS headers
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+        if bottle.request.method != 'OPTIONS':
+            # actual request; reply with the actual response
+            return fn(*args, **kwargs)
+
+    return _enable_cors
 
 @app.post('/')
+@enable_cors
 def request_to_serial():
     try:
-        payload = request.forms.get('payload')
+        payload = request.json()['payload']
+        return {'response': payload}
         response = send_payload_to_serial(payload)
         return {'response': response}
     except PayloadTooBigException:
@@ -39,7 +61,7 @@ def send_payload_to_serial(payload:str) -> str:
 
 def parse_args():
     arg_parser = argparse.ArgumentParser(description='Forwards 0 terminated messages between HTTP and serial port')
-    arg_parser.add_argument('--serialport', '-s', help='Serial port', metavar='PORT', required=True, type=str)
+    arg_parser.add_argument('--serialport', '-s', help='Serial port', metavar='PORT', default=find_device_port(), type=str)
     arg_parser.add_argument('--baudrate', '-b', help='Serial baud rate', metavar='RATE', default=115200, type=int)
     return arg_parser.parse_args()
 
@@ -51,4 +73,4 @@ if __name__ == "__main__":
     ser.baudrate = args.baudrate
     ser.open()
 
-    run(app, host='localhost', port=8080)
+    run(app, host='localhost', port=9876)
