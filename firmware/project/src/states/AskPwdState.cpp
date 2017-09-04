73f06e80db16f52767039e7175f337f8440e6436
Da Silva Bruno <dasilvabruno86@hotmail.fr>
2017-09-04 18:30:54 +0200

    Wait and Domain

--- firmware/project/src/states/AskPwdState.cpp
+++ firmware/project/src/states/AskPwdState.cpp
@@ -4,37 +4,44 @@
 #include "TouchManager.h"
 #include "colors.h"
 #include "comunication.h"
+#include <string.h>
 
 #define BACKGROUND ANDROID_KB_BG
 #define MESSAGE_BACKGROUND ANDROID_KB_KEY
 
 const int BUTTONS_SIZE[2] = {100, 50};
-const int BUTTONS_POS[2][2] = {{50, 150}, {170, 150}};  // j'ai déplacé tes boutons de 10 pixels à droite, ils n'étaient pas centrés
-const unsigned int BUTTONS_COLOR[2] = {RED, GREEN};
+const int BUTTONS_POS[2][2] = {{50, 150}, {170, 150}}; // j'ai déplacé tes boutons de 10 pixels à droite, ils n'étaient pas centrés
+const unsigned int BUTTONS_COLOR[2] = {RED, DARKGREEN};
 
 char *msg;
+int len, center_position;
+
 AskPwdState::AskPwdState()
 {
     msg = communication_read();
     tft.fillScreen(BACKGROUND);
-   
+
     tft.fillRoundRect(30, 40, 260, 40, 4, MESSAGE_BACKGROUND);
     tft.setCursor(43, 52);
     tft.setTextColor(WHITE);
     tft.setTextSize(2);
     tft.setTextWrap(false);
-    tft.print("Request received for");
+    tft.print("Password asked:");
+
+    len = strlen(msg);
+    center_position = ((260 -len * 12) / 2) + 30;
 
     tft.fillRoundRect(30, 90, 260, 40, 4, MESSAGE_BACKGROUND);
-    tft.setCursor(100, 102);
-    tft.setTextColor(WHITE);
+    tft.setCursor(center_position, 102);
+    tft.setTextColor(ORANGE);
     tft.setTextSize(2);
     tft.setTextWrap(false);
     tft.print(msg);
 
-    for (int i = 0; i < 2; i++){
+    for (int i = 0; i < 2; i++)
+    {
         int *pos = BUTTONS_POS[i];
-        tft.fillRoundRect(pos[0], pos[1], BUTTONS_SIZE[0], BUTTONS_SIZE[1], 3, BUTTONS_COLOR[i]);       
+        tft.fillRoundRect(pos[0], pos[1], BUTTONS_SIZE[0], BUTTONS_SIZE[1], 3, BUTTONS_COLOR[i]);
     }
     tft.setCursor(65, 168);
     tft.setTextColor(WHITE);
@@ -46,7 +53,7 @@ AskPwdState::AskPwdState()
     tft.setTextColor(WHITE);
     tft.setTextSize(2);
     tft.setTextWrap(false);
-    tft.print("Ok"); 
+    tft.print("Ok");
 }
 
 int AskPwdState::loop()
